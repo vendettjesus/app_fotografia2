5fc29afbf244c42d4115d6b9d2b2e3f1ea6090c0
José de Jesús Alvarado Castillo <jesusalvarado@outlook.com>
2023-01-30 11:48:35 -0600

    App_fotografia_angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'', component:InicioComponent},
+  {path:'inicio', component:InicioComponent},
+  {path:'portafolio', component: PortafolioComponent},
+  {path:'servicios', component: ServiciosComponent},
+  {path:'contactanos', component: ContactanosComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MenuComponent } from './menu/menu.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+import { ServiciosComponent } from './servicios/servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent,
+    InicioComponent,
+    MenuComponent,
+    ContactanosComponent,
+    ServiciosComponent
   ],
   imports: [
     BrowserModule,
